ddfa74809d27e289c7aef71fb80a4a74aa84d3ac
Ilka Pleiser <ilka.pleiser@web.de>
2021-01-18 16:01:13 +0100

    FAQ's and Start Site

--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -8,36 +8,49 @@ import background from "../img/pexels-lisa-fotios-1933386.jpg"
 //import bgimage from "../../src/img/pexels-lisa-fotios-1933386.jpg";
 import bgimage from "../../src/img/pexels-emmet-128421.jpg";
 import { HashLink } from 'react-router-hash-link';
+import { BreakpointProvider } from 'react-socks';
 
 const Container = () => {
-  
-  return (
 
-    
-    <Jumbotron className="jumbotron-container" style={{
+return (
+<BreakpointProvider small>
+
+<Jumbotron className="jumbotron-container" style={{
       backgroundImage: `url(${bgimage})`,
       backgroundSize: "cover",
       backgroundAttachment: "fixed",
       marginBottom: "0px"      
     }}>
 
-        <h1> Waste is a recurring topic in the public discussion.<br/> It is often forgotten otherwise.</h1> <br/>
-    <div className="column">
-         <HashLink smooth to="/#test"><Button variant="primary">Show me the facts</Button></HashLink>
-        
+  <div className='container'>
+    <div className='row'>
+      <div style={{textAlign:'center'}}>
+        <h1 style={{fontSize:'3vw'}}>Waste is a recurring topic in the public discussion.</h1><br/> 
+        <p style={{fontSize:'2vw'}}>It is often forgotten otherwise.</p>
+      </div>
+    </div>
+
+
+    <div className='container' style={{marginTop:'200px'}}>
+      <div className='row'>
+        <div className='column'>
+          <HashLink smooth to="/#test"><Button variant="primary" size="lg" block>Story</Button></HashLink>
         </div>
         <div className="column">
-         <HashLink smooth to="/#test"><Button variant="primary">Show me the facts</Button></HashLink> 
+          <HashLink smooth to="/map"><Button variant="primary" size="lg" block>Map</Button></HashLink>
         </div>
         <div className="column">
-         <HashLink smooth to="/#test"><Button variant="primary">Show me the facts</Button></HashLink>        
+          <HashLink smooth to="/#test"><Button variant="primary" size="lg" block>Story Map</Button></HashLink>
         </div>
-        
-      </Jumbotron>
+      </div>
+    </div>
+  </div>
 
-    
+</Jumbotron>
 
-  );
+</BreakpointProvider>
+
+);
 };
 
 export default Container;
