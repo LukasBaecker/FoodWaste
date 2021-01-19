5fbfc102a5ef1fbab3403d860c9390f63b6fdb19
Ilka Pleiser <ilka.pleiser@web.de>
2021-01-19 14:51:39 +0100

    Tiny changes

--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -8,12 +8,12 @@ import background from "../img/pexels-lisa-fotios-1933386.jpg"
 //import bgimage from "../../src/img/pexels-lisa-fotios-1933386.jpg";
 import bgimage from "../../src/img/pexels-emmet-128421.jpg";
 import { HashLink } from 'react-router-hash-link';
-import { BreakpointProvider } from 'react-socks';
+import { Breakpoint, BreakpointProvider } from 'react-socks';
 
 const Container = () => {
 
 return (
-<BreakpointProvider small>
+
 
 <Jumbotron className="jumbotron-container" style={{
       backgroundImage: `url(${bgimage})`,
@@ -48,7 +48,6 @@ return (
 
 </Jumbotron>
 
-</BreakpointProvider>
 
 );
 };
