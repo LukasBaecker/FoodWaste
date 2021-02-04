3e280a73b0170aa274024281157563b6f1102a23
JuanFFranco <jfranco@uni-muenster.de>
2021-02-04 16:46:17 +0100

    Change Europe color ramp

--- src/entities/LegendItems.js
+++ src/entities/LegendItems.js
@@ -3,29 +3,29 @@ import LegendItem from "./LegendItem"
 const legendItems = [
     new LegendItem(
         "6.746.343 +",
-        "#bd0026",
+        "#00441b",
         (quantity) => quantity >= 6746343,
         "white",
     ),
     new LegendItem(
         "2.591.187 - 6.746.343",
-        "#f03b20",
+        "#267b3a",
         (quantity) => quantity >= 2591187 && quantity< 6746343,
         "white",
     ),
     new LegendItem(
         "1.478.966- 2.591.187",
-        "#fd8d3c",
+        "#06aa6e",
         (quantity) => quantity >= 1478966 && quantity< 2591187,
     ),
     new LegendItem(
         "408.103 - 1.478.966",
-        "#fecc5c",
+        "#caeac3",
         (quantity) => quantity >= 408103 && quantity< 1478966,
     ),
     new LegendItem(
         "0 - 408.103",
-        "#ffffb2",
+        "#f7fcf5",
         (quantity) => quantity >0 && quantity< 408103,
     ),
     new LegendItem ("No Data", "#ffffff", (quantity) => true),
